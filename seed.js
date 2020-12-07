37eac7883e18d3a40b793e73dc2cf9680d997942
Erick Canali <erick.canali@gmail.com>
2020-12-07 08:18:38 -0300

    favicon and more placeholder images to seed.js added

--- seed.js
+++ seed.js
@@ -5,68 +5,84 @@ const User = require('./src/app/models/User')
 const Product = require('./src/app/models/Product')
 const File = require('./src/app/models/File')
 
-let usersIds = []
-let totalProducts = 10
-let totalUsers = 3
+let usersIds = [],
+    totalUsers = 5,
+    totalProducts = 10,
+    totalProductsImages = 50
 
 async function createUsers() {
-    const users = []
-    const password = await hash('1111', 8)
-
-    while (users.length < totalUsers) {
-        users.push({
-            name: faker.name.firstName(),
-            email: faker.internet.email(),
-            password,
-            cpf_cnpj: faker.random.number(99999999999),
-            cep: faker.random.number(99999999),
-            address: faker.address.streetName()
-        })
+    try {
+        const users = []
+        const password = await hash('1111', 8)
+    
+        while (users.length < totalUsers) {
+            users.push({
+                name: faker.name.firstName(),
+                email: faker.internet.email(),
+                password,
+                cpf_cnpj: faker.random.number(99999999999),
+                cep: faker.random.number(99999999),
+                address: faker.address.streetName()
+            })
+        }
+    
+        const usersPromise = users.map(user => User.create(user))
+    
+        usersIds = await Promise.all(usersPromise)
+        
+    } catch (error) {
+        console.error(error)
     }
-
-    const usersPromise = users.map(user => User.create(user))
-
-    usersIds = await Promise.all(usersPromise)
 }
 
 async function createProducts() {
-    let products = []
-
-    while (products.length < totalProducts) {
-        products.push({
-            category_id: Math.ceil(Math.random() * 3),
-            user_id: usersIds[Math.floor(Math.random() * totalUsers)],
-            name: faker.name.title(),
-            description: faker.lorem.paragraph(Math.ceil(Math.random() * 10)),
-            old_price: faker.random.number(9999),
-            price: faker.random.number(9999),
-            quantity: faker.random.number(99),
-            status: Math.round(Math.random())
-        })
-    }
+    try {
+        let products = []
     
-    const productsPromise = products.map(product => Product.create(product))
-
-    productsIds = await Promise.all(productsPromise)
+        while (products.length < totalProducts) {
+            products.push({
+                category_id: Math.ceil(Math.random() * 3),
+                user_id: usersIds[Math.floor(Math.random() * totalUsers)],
+                name: faker.name.title(),
+                description: faker.lorem.paragraph(Math.ceil(Math.random() * 10)),
+                old_price: faker.random.number(9999),
+                price: faker.random.number(9999),
+                quantity: faker.random.number(99),
+                status: 1
+            })
+        }
+        
+        const productsPromise = products.map(product => Product.create(product))
     
-    let files = []
-
-    while (files.length < 50) {
-        files.push({
-            name: faker.image.image(),
-            path: `public/images/placeholder.png`,
-            product_id: productsIds[Math.floor(Math.random() * totalProducts)]
-        })
+        const productsIds = await Promise.all(productsPromise)
+        
+        let files = []
+    
+        while (files.length < totalProductsImages) {
+            files.push({
+                name: faker.image.image(),
+                path: `public/images/product-placeholder${Math.ceil(Math.random() * 9)}.png`,
+                product_id: productsIds[Math.floor(Math.random() * totalProducts)]
+            })
+        }
+    
+        const filesPromise = files.map(file => File.create(file))
+    
+        await Promise.all(filesPromise)
+        
+    } catch (error) {
+        console.error(error)
     }
-
-    const filesPromise = files.map(file => File.create(file))
-
-    await Promise.all(filesPromise)
 }
 
 async function init() {
-    await createUsers()
-    await createProducts()
+    try {
+        await createUsers()
+        await createProducts()
+
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 init()
